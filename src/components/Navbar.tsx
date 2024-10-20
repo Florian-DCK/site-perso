0d51f9db17ce546f83d64e24cdeb93b2ad301e91
Florian Donckers <fdonckersf@gmail.com>
2024-10-21 01:43:02 +0200

    correction des erreurs eslint

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,25 +7,25 @@ import Link from 'next/link';
 const tabStyle: MotionStyle = {
     width: '100px',
     textAlign: 'center',
-    position: 'relative' as 'relative', // Explicitly type-casting
+    position: 'relative' as const, // Explicitly type-casting
     borderRadius: 10,
     padding: '0.5rem',
-};
+} as const;
 
 const selectionStyle: MotionStyle = {
     width: '100%',
     height: '100%',
-    position: 'absolute' as 'absolute', // Explicitly type-casting
+    position: 'absolute' as const, // Explicitly type-casting
     borderRadius: 1000,
     top: 0,
     left: 0,
-};
+} as const;
 
 const tabs = [
     { name: 'Accueil', path: '/' },
     { name: 'CV', path: '/cv' },
     { name: 'contact', path: '/contact' },
-];
+] as const;
 
 const duration = 0.5;
 
@@ -35,7 +35,7 @@ export default function NavBar() {
     return (
         <div className=' fixed top-0 flex  h-fit max-h-[calc(100vh-40px)] py-2 min-w-fit w-fit justify-center bg-background items-center m-5 rounded-full   drop-shadow-2xl px-5 backdrop-blur-md bg-opacity-80'>
             {tabs.map(({ name, path }, i) => (
-                <Link style={{ position: "relative", zIndex: 1 }} href={path}>
+                <Link style={{ position: "relative", zIndex: 1 }} href={path} key={`${i}-parent`}>
                     <motion.div
                         style={tabStyle}
                         key={i}
