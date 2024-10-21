74482ee131492a07b6c950cec0d395066760bce4
Florian Donckers <fdonckersf@gmail.com>
2024-10-21 04:22:59 +0200

    Removed SlideInLeftDelayed

--- src/components/Transitions.tsx
+++ src/components/Transitions.tsx
@@ -39,24 +39,6 @@ const SlideInLeft = ({ children, delay=0 }: { children: ReactNode, delay?: numbe
   );
 };
 
-const SlideInLeftDelayed = ({ children }: { children: ReactNode }) => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
-
-  return (
-    <motion.div
-      ref={ref}
-      style={{
-        transform: isInView ? 'none' : 'translateX(-200px)',
-        opacity: isInView ? 1 : 0,
-        transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.9s',
-      }}
-    >
-      {children}
-    </motion.div>
-  );
-};
-
 const DisapearOnScroll = ({ children }: { children: ReactNode }) => {
   const ref = useRef(null);
   const isInView = useInView(ref);
@@ -79,4 +61,4 @@ const DisapearOnScroll = ({ children }: { children: ReactNode }) => {
   );
 }
 
-export { FadeIn, SlideInLeft , SlideInLeftDelayed, DisapearOnScroll };
+export { FadeIn, SlideInLeft, DisapearOnScroll };
