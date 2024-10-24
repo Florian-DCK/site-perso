38c3b0142c99984d74c5964e2a29a2eade7ff97a
Florian Donckers <fdonckersf@gmail.com>
2024-10-24 16:22:07 +0200

    WIP

--- src/components/DecorationAsciiAscii.tsx
+++ src/components/DecorationAsciiAscii.tsx
@@ -1,18 +1,34 @@
-import { DisapearOnScroll } from './Transitions';
-import { ScrollDown } from '@/components/svgs/index';
+'use client'
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function DecorationAscii() {
+
+	useGSAP(() => {
+		gsap.to('.DecorationAscii', { 
+			x: 50,
+			y: 50,
+			opacity: 0,
+			ease: 'ease-in-out',
+			scrollTrigger: {
+				trigger: '.DecorationAscii',
+				start: 'top 90%',
+				markers: false,
+				toggleActions: 'play none none reverse'
+			}
+		})
+	})
+
 	return (
-		<div className=" absolute bottom-0 r w-[calc(100vw-3rem)] h-full flex items-center justify-center mx-5 [&_*]:text-accent [&_*]:text-[2rem] overflow-hidden select-none pointer-events-none">
-			<DisapearOnScroll>
-				<pre className=" text-center self-center overflow-hidden absolute bottom-0 left-0">
-					{Array(300).fill('-').join('')}
+		<div className=" DecorationAscii absolute bottom-0 ">
+			  		{Array(300).fill('-').join('')}
 				</pre>
 
 				<div className="absolute bottom-10 [&_*]:fill-accent [&_*]:size-10 animate-bounce">
-					<ScrollDown></ScrollDown>
 				</div>
-			</DisapearOnScroll>
 		</div>
 	);
 }
